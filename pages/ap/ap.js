182ee621f9d79185129e0607445411bf9c16c9d0
lch <lch872@qq.com>
2018-01-26 20:10:12 +0800

    first

---
+++ pages/ap/ap.js
@@ -0,0 +1,102 @@
+// pages/ap/ap.js
+const app = getApp()
+var order = ['red', 'yellow', 'blue', 'green', 'red']
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    toView: 'red',
+    scrollTop: 100,
+    isScroll: false,
+    index: 0,
+    array: ['请选择活动时间', '下午14:00 - 下午18:00', '晚上19:00 - 晚上21:00'],
+  },
+
+  bindViewTap: function () {
+    wx.request({
+      url: 'http://localhost:8080/wx/apply',
+      method: 'POST',
+      data: app.globalData.userInfo,
+      header: {
+        'content-type': 'application/x-www-form-urlencoded' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+        console.log(app.globalData.userInfo)
+        
+      }
+    })
+  },
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+  tap: function (e) {
+    for (var i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1]
+        })
+        break
+      }
+    }
+  },
+  tapMove: function (e) {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
+    })
+  }
+})
